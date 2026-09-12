990b88bb8f3d73dae43dd01224d158eac4bd5537
SANTHOSH S <santhoshsan151087@gmail.com>
2026-09-12 11:57:32 +0530

    Time: 0 ms (100%), Space: 8 MB (66.64%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     #define ll long long int
- 
+    vector<int> dp;
     ll cat(ll n){
        if(n<=1)
        return 1;
+       if(dp[n]!=-1)
+       return dp[n];
        ll res = 0;
        for(int i=0;i<n;i++){
         res+=(cat(i) * cat(n-i-1));
        }
-       return res;
+       return dp[n]=res;
     }
     int numTrees(int n) {
+        dp.resize(n+1 , -1);
         return (int)cat(n);
     }
 };
