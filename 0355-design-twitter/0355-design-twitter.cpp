f24118ca31206c25fcf6fdf7159b51b96f31406e
SANTHOSH S <santhoshsan151087@gmail.com>
2026-09-23 19:15:27 +0530

    Time: 24 ms (12.02%), Space: 58.1 MB (13.91%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,47 +1,76 @@
 class Twitter {
 public:
-    vector<pair<int , int>> st;
-     vector<vector<bool>> followed;
+    int time = 0;
+
+    // user -> list of {time, tweetId}
+    vector<vector<pair<int, int>>> tweets;
+
+    // follower -> followees
+    vector<vector<bool>> followed;
+
     Twitter() {
-        
-        followed.resize(501 , vector<bool>(501, false));
+        tweets.resize(501);
+        followed.resize(501, vector<bool>(501, false));
     }
-    
+
     void postTweet(int userId, int tweetId) {
-        st.push_back({userId , tweetId});
+        tweets[userId].push_back({time++, tweetId});
     }
-    
+
     vector<int> getNewsFeed(int userId) {
         vector<int> ans;
-        int count = 0;
-        for(int i=st.size()-1;i>=0 && count<10;i--){
-            // cout<<st[i].first<<" ";
-            // cout<<st[i].second<<" ";
-            
-            if(userId == st[i].first || followed[userId][st[i].first]){
-                ans.push_back(st[i].second);
-                count++;
+
+        // max heap:
+        // {time, tweetId, userId, index}
+        priority_queue<
+            tuple<int, int, int, int>
+        > pq;
+
+        // User's own tweets
+        if (!tweets[userId].empty()) {
+            int i = tweets[userId].size() - 1;
+            auto [t, id] = tweets[userId][i];
+            pq.push({t, id, userId, i});
+        }
+
+        // Tweets from followed users
+        for (int followee = 1; followee <= 500; followee++) {
+            if (followed[userId][followee] && !tweets[followee].empty()) {
+                int i = tweets[followee].size() - 1;
+                auto [t, id] = tweets[followee][i];
+                pq.push({t, id, followee, i});
             }
         }
+
+        // Get latest 10 tweets
+        while (!pq.empty() && ans.size() < 10) {
+            auto [t, tweetId, user, index] = pq.top();
+            pq.pop();
+
+            ans.push_back(tweetId);
+
+            // Get previous tweet from the same user
+            if (index > 0) {
+                int newIndex = index - 1;
+                auto [newTime, newTweetId] = tweets[user][newIndex];
+
+                pq.push({
+                    newTime,
+                    newTweetId,
+                    user,
+                    newIndex
+                });
+            }
+        }
+
         return ans;
     }
-    
+
     void follow(int followerId, int followeeId) {
         followed[followerId][followeeId] = true;
-        // followed[followeeId][followerId] = true;
     }
-    
+
     void unfollow(int followerId, int followeeId) {
-         followed[followerId][followeeId] = false;
-        //   followed[followeeId][followerId] = false;
+        followed[followerId][followeeId] = false;
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
